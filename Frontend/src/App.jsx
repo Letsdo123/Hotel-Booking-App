2ae0ba66c9bb429afc2c1436fa1882f69fc00ce1
ayan-dev25 <developer.ayan25@gmail.com>
2024-07-23 09:58:39 +0530

    add input and calender component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,67 @@
 import './App.css'
 import { Button } from '@material-tailwind/react'
 import { NavMenu } from './components/header/NavMenu'
+import CustomInput from './components/utils/CustomInput'
+import CustomDateRangePicker from './components/utils/CustomDateRangePicker'
 
 function App() {
+
+  const inputJson = [
+    {
+      id: 1,
+      label: "Password",
+      type: "password",
+      placeholder: "Password",
+      disabled: false,
+      inputDescription: "8 chars"
+    },
+    {
+      id: 2,
+      label: "Username",
+      type: "text",
+      placeholder: "Username",
+      disabled: false,
+      inputDescription: "6 chars"
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "email",
+      placeholder: "Email",
+      disabled: true
+    },
+  ]
+
   return (
     <>
-    <NavMenu />
-      <Button>Button</Button>;
+      <div className='container-fluid'>
+        <NavMenu />
+        <div className='grid grid-cols-2 gap-4 justify-items-center'>
+          <div className='m-2'>
+            {inputJson.map(({ id, label, type, placeholder,inputDescription, disabled,onChange }) => (
+              <CustomInput
+                key={id}
+                id={id}
+                label={label}
+                type={type}
+                placeholder={placeholder}
+                inputDescription={inputDescription}
+                disabled={disabled}
+                onChange={onChange && onChange}
+              />
+            ))}
+            <CustomDateRangePicker />
+          </div>
+          
+          <div className='m-2'>
+            <Button>Button</Button>
+          </div>
+
+        </div>
+      </div>
+
+
+
     </>
   )
 }
---
+++ Frontend/src/components/utils/CustomDateRangePicker.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { addDays } from 'date-fns';
+import { useState } from 'react';
+
+function CustomDateRangePicker() {
+    const [state, setState] = useState([
+        {
+            startDate: new Date(),
+            endDate: addDays(new Date(), 7),
+            key: 'selection'
+        }
+    ]);
+    return (
+        <div>
+            <DateRangePicker
+                onChange={item => setState([item.selection])}
+                showSelectionPreview={true}
+                moveRangeOnFirstSelection={false}
+                months={2}
+                ranges={state}
+                direction="horizontal"
+            />
+        </div>
+    )
+}
+
+export default CustomDateRangePicker
+
+
+
+
+
